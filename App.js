5e860796077c51065c5729fd92021c2f64436410
Nigel Powell <nigel@thesadsongco.com>
2021-12-09 18:17:41 +0000

    KoHo font installed - bravo

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, KoHo_400Regular } from '@expo-google-fonts/koho';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Outer Town 2021</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    KoHo_400Regular,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontFamily: 'KoHo_400Regular', fontSize: 40 }}>Outer Town 2021</Text>
+      </View>
+    );
+  }
+}
---
+++ Styles/baseStyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const baseStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f4f4ec',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },    
+})
