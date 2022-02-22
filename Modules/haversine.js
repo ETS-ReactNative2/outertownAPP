d4655ea6131d721480f758e549c371bd9c39368f
Nigel Powell <nigel@thesadsongco.com>
2022-02-22 19:29:41 +0000

    haversine module unit tests

--- Modules/haversine.js
+++ Modules/haversine.js
@@ -9,24 +9,29 @@ export default function haversineDistance(coords1, coords2) {
     function toRad(x) {
       return x * Math.PI / 180;
     }
-  
-    var lon1 = coords1.longitude;
-    var lat1 = coords1.latitude;
-  
-    var lon2 = coords2.longitude;
-    var lat2 = coords2.latitude;
-  
-    var R = 6371; // km
-  
-    var x1 = lat2 - lat1;
-    var dLat = toRad(x1);
-    var x2 = lon2 - lon1;
-    var dLon = toRad(x2)
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    try {
+
+      var lon1 = coords1.longitude;
+      var lat1 = coords1.latitude;
+      
+      var lon2 = coords2.longitude;
+      var lat2 = coords2.latitude;
+      
+      var R = 6371; // km
+      
+      var x1 = lat2 - lat1;
+      var dLat = toRad(x1);
+      var x2 = lon2 - lon1;
+      var dLon = toRad(x2)
+      var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c * 1000; // in meters
-  
-    return d;
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c * 1000; // in meters
+      
+      return d;
+    }
+    catch (e) {
+      return false;
+    }
   }
---
+++ __tests__/Modules/getBandInfo.test.js
@@ -0,0 +1,8 @@
+import auto from 'fake-indexeddb';
+
+describe("sample tests", () => {
+    it("5*5 should be 25", () => {
+        expect(5*5).toBe(25);
+    });
+})
+
